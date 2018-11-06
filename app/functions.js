c167a99ea69307f78be74b18821ac9f1c5adebed
vkservicesllc <35149642+vkservicesllc@users.noreply.github.com>
2018-11-06 08:38:28 -0500

    update 1811060838

--- app/functions.js
+++ app/functions.js
@@ -207,7 +207,7 @@ var monthlyUrls = {
       name: '<th>CAROLINA TRANSPORTATION &nbsp<span>70830515178290</span></th>',
       month1_cards: {
         user0:     [0, '686342087'],
-        user1:     [1, '1995296890'],
+        user1:     [0, '1995296890'],
         user2:     [0, '2117669368'],
         month_key: ['Oct', '1n9RDORRFa7HgjHqKE_ELi-vKA75IVzMNpOYYsuWkxJQ']
       },
