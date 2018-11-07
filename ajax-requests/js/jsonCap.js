2ffae59b9b4287f2af79da80c1d9dbfdce987db5
Val <35149642+vkservicesllc@users.noreply.github.com>
2018-11-07 15:24:58 -0500

    Update jsonCap.js

--- ajax-requests/js/jsonCap.js
+++ ajax-requests/js/jsonCap.js
@@ -3,26 +3,28 @@ console.log('jsonCap.js is running...');
 function captureJSON() {
   var xhr = new XMLHttpRequest();
   xhr.onload = function() {
-    if (xhr.status >= 200 && xhr.status < 400) {
-      var div = document.getElementById('capturedJSON'),
-          content = '',
-          respObj = JSON.parse(xhr.responseText),
-          len = respObj.length,
-          i;
-      console.log(xhr.responseText);
-      content += '<table><tr><th>Company</th>';
-      content += '<th>First Name</th><th>Last Name</th><th>Title</th></tr>';
-      for (i = 0; i < len; i++) {
-        content += '<tr><td>' + respObj[i].Company + '</td>';
-        content += '<td>' + respObj[i]['First Name'] + '</td>';
-        content += '<td>' + respObj[i]['Last Name'] + '</td>';
-        content += '<td>' + respObj[i].Title + '</td></tr>';
-        if (i == len) content += '</table>';
+    var div = document.getElementById('capturedJSON'),
+        content = '',
+        check = div.innerHTML = content;
+    if (check) {
+      if (xhr.status >= 200 && xhr.status < 400) {
+        var respObj = JSON.parse(xhr.responseText),
+            len = respObj.length,
+            i;
+        console.log(xhr.responseText);
+        content += '<table><tr><th>Company</th>';
+        content += '<th>First Name</th><th>Last Name</th><th>Title</th></tr>';
+        for (i = 0; i < len; i++) {
+          content += '<tr><td>' + respObj[i].Company + '</td>';
+          content += '<td>' + respObj[i]['First Name'] + '</td>';
+          content += '<td>' + respObj[i]['Last Name'] + '</td>';
+          content += '<td>' + respObj[i].Title + '</td></tr>';
+          if (i == len) content += '</table>';
+        }
+        div.innerHTML = content;
+      } else {
+        console.log('Connection to Server succeeded. Content was not received due to error.');
       }
-      if (div.innerHTML != '') content = '';
-      div.innerHTML = content;
-    } else {
-      console.log('Connection to Server succeeded. Content was not received due to error.');
     }
   };
   xhr.open('GET', 'files/jsonData.json', true);
