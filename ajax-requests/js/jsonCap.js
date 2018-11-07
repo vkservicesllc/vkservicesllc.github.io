3066366d0949de69f3e3e22dcf3a2ff8f33fea0c
vkservicesllc <35149642+vkservicesllc@users.noreply.github.com>
2018-11-07 14:19:01 -0500

    Update jsonCap.js

--- ajax-requests/js/jsonCap.js
+++ ajax-requests/js/jsonCap.js
@@ -1,1 +1,28 @@
-
+function captureJSON() {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = funtion() {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      var div = document.getElementById('capturedJSON'),
+          content = '',
+          respObj = JSON.parse(xhr.responseText),
+          len = respObj.length,
+          var i;
+      console.log(xhr.responseText);
+      content += '<table><tr><th>Company</th>';
+      content += '<th>First Name</th><th>Last Name</th><th>Title</th></tr>';
+      for (i = 0; i < len; i++) {
+        content += '<tr><td>' + respObj[i].Company + '</td>';
+        content += '<td>' + respObj[i]['First Name'] + '</td>';
+        content += '<td>' + respObj[i]['Last Name'] + '</td>';
+        content += '<td>' + respObj[i].Title + '</td></tr>';
+        if (i == len) content += '</table>';
+      }
+      if (div.innerHTML != '') content = '';
+      div.innerHTML = content;
+    } else {
+      console.log('Connection to Server succeeded. Content was not received due to error.');
+    }
+  };
+  xhr.open('GET', '../files/jsonData.json', true);
+  xhr.send();
+}
