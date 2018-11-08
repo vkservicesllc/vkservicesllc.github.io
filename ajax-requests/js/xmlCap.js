1b97f795866842b277d6e5c8f08bdbe530214829
Val <35149642+vkservicesllc@users.noreply.github.com>
2018-11-08 09:40:44 -0500

    Update xmlCap.js

--- ajax-requests/js/xmlCap.js
+++ ajax-requests/js/xmlCap.js
@@ -11,8 +11,16 @@ function captureXML() {
         check = div.innerHTML == content;
     if (check) {
       if (xhr.status >= 200 && xhr.status < 400) {
-        var data = xhr.responseXML;
-        
+        var data = xhr.responseXML.getElementsByTagNames('expense');
+        content += '<table><tr><th colspan="3">COMPANY EXPENSE</th></tr>';
+        content += '<tr><th>Category</th><th>Group</th>File<br>for<br>Tax</th></tr>';
+        for (var d = 0; d < data.length; d++) {
+          var checked = (data[d].childNodes[2].textContent == 'true') ? ' checked' : '';
+          content += '<tr><td>' + data[d].childNodes[0].textContent + '</td>';
+          content += '<td>' + data[d].childNodes[1].textContent + '</td>';
+          content += '<td><input type="checkbox"' + checked + '></td></tr>';
+        }
+        content += '</table>';
       }
         btnTxt = btnTxt.replace('capture', 'hide');
       } else {
