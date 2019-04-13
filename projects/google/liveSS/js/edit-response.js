fe919ee97359679b196975516f8126c4aacbdac0
Val <35149642+vkservicesllc@users.noreply.github.com>
2019-04-13 09:40:56 -0400

    update 1904130940

---
+++ projects/google/liveSS/js/edit-response.js
@@ -0,0 +1,16 @@
+/**
+ *  Updated:  2019-04-12
+ */
+ 
+
+/** Edit Response **/
+
+function executeEditResponse(eventDesc) {
+  /*
+   *  arguments:  eventDesc
+   *
+   *  eventDesc  ->  Edit Event description object
+   *
+   */
+
+}
