18acd2fd63cd10e89fa52009530ae4b98e2f9284
vkservicesllc <35149642+vkservicesllc@users.noreply.github.com>
2019-07-25 15:57:44 -0400

    update 1907251557

---
+++ app-new/mc-owners-ss.js
@@ -0,0 +1,37 @@
+const monthlySpreadsheets = {
+
+  /*
+
+  'yyyy-mmmm': {
+
+    'CLs': ['', 1],
+    'DE': ['', 1],
+    'DL': ['', 1],
+    'OT': ['', 1],
+    'PT': ['', 1],
+
+  },
+
+  */
+
+  '2019-August': {
+
+    'CLs': ['1QNYGrVphn1D7Bajr9m2vuZSoC_G2Pu0yf_CBfahhplE', 1],
+    'DE': ['1MpWH4hVVALr-W21pG8JJ0TXtQYaFjpFjYStjRnHeF2o', 1],
+    'DL': ['17vjSYZt2iP7XcjJRAjDS2a9qUY4IMVfU-SX9RpTx-Yg', 1],
+    'OT': ['11xLawls0LBEwRpp5lc130-nun2-Guh-7Gym7RSQ_-XU', 1],
+    'PT': ['1EDk1za4PVYkUEgUVvjCH1CCEo1mtI5FXC-f7fhUMz8k', 1],
+
+  },
+
+  '2019-July': {
+
+    'CLs': ['1r4kELYN33dsoxD6IGcivMsFGKSP5Qebci0iR8fMGOsI', 1],
+    'DE': ['12GULr80IX1HED0_ccG7fkaT4YZb_nrZKfGGDd5Vjy1U', 1],
+    'DL': ['1emjsj1mO4LqvKSBkWjWQQNmpQcrJQJauYsHW-0n7BT4', 1],
+    'OT': ['1QyFoBuVNdVvXiCqsiRF9vAg4Wzph0q6NwhgrNpfcqA4', 1],
+    'PT': ['11Hq6FpGAY8LTsdMgOiLjZJl2dwEa97A0eHpu1UNEEog', 1],
+
+  },
+
+};
