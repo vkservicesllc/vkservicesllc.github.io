b84098b3735255b7eca1ac93584eafd7484c6710
vkservicesllc <35149642+vkservicesllc@users.noreply.github.com>
2019-09-27 10:09:10 -0400

    update 1909271009

--- app-new/mc-owners-ss.js
+++ app-new/mc-owners-ss.js
@@ -2,45 +2,55 @@ const monthlySpreadsheets = {
 
   /*
 
-  'yyyy-mmmm': {
+  '20yy-mmmm': {
 
     'CLs': ['', 1],
-    'DE': ['', 1],
-    'DL': ['', 1],
-    'OT': ['', 1],
-    'PT': ['', 1],
+    'DE':  ['', 1],
+    'DL':  ['', 1],
+    'OT':  ['', 1],
+    'PT':  ['', 1],
 
   },
 
   */
 
+  '2019-October': {
+
+    'CLs': ['1Gs97P_EaIkCznL-Z6jUZ3FVw-x1GDxcCfiiI6kuAtiY', 1],
+    'DE':  ['19LwvscCJKMmCUka1I2abZegLqEVQpd8fVsFbhFY6ToA', 1],
+    'DL':  ['15HisvxlimpbEpb1rQ4Zo7NdZWl27t3qIEo83L_yAPkE', 1],
+    'OT':  ['1Zju9fvZ9oJ6mALPffNHEeTXUCxbzWiFuC4E0bWuHzrU', 1],
+    'PT':  ['1ITsbjfL4A1HVCia85Q1urfquLKrFK3GpIseqwV_PYNs', 1],
+
+  },
+
   '2019-September': {
 
     'CLs': ['1nHRrll7JwNyy5DocKO7PfO5ygbXWTDB8hZe-M9dYuz4', 1],
-    'DE': ['12d4_F7M6w_Gbi_lgq0AZV5BGLOzWzGsT3DuMpJGjYh4', 1],
-    'DL': ['1l-uDB8lEJuwutDoYeVVjs8dBfDgCLc5LozHrw1bFB08', 1],
-    'OT': ['1_GCL40TdGZPGgxpsQFAOrXS6BLB-YVvrMMyTVX2hkPc', 1],
-    'PT': ['1ZrD5Z58zicS2K4X7r9-dDGRnJFKVkg8mapZgv7EsA7c', 1],
+    'DE':  ['12d4_F7M6w_Gbi_lgq0AZV5BGLOzWzGsT3DuMpJGjYh4', 1],
+    'DL':  ['1l-uDB8lEJuwutDoYeVVjs8dBfDgCLc5LozHrw1bFB08', 1],
+    'OT':  ['1_GCL40TdGZPGgxpsQFAOrXS6BLB-YVvrMMyTVX2hkPc', 1],
+    'PT':  ['1ZrD5Z58zicS2K4X7r9-dDGRnJFKVkg8mapZgv7EsA7c', 1],
 
   },
 
   '2019-August': {
 
     'CLs': ['1QNYGrVphn1D7Bajr9m2vuZSoC_G2Pu0yf_CBfahhplE', 1],
-    'DE': ['1MpWH4hVVALr-W21pG8JJ0TXtQYaFjpFjYStjRnHeF2o', 1],
-    'DL': ['17vjSYZt2iP7XcjJRAjDS2a9qUY4IMVfU-SX9RpTx-Yg', 1],
-    'OT': ['11xLawls0LBEwRpp5lc130-nun2-Guh-7Gym7RSQ_-XU', 1],
-    'PT': ['1EDk1za4PVYkUEgUVvjCH1CCEo1mtI5FXC-f7fhUMz8k', 1],
+    'DE':  ['1MpWH4hVVALr-W21pG8JJ0TXtQYaFjpFjYStjRnHeF2o', 1],
+    'DL':  ['17vjSYZt2iP7XcjJRAjDS2a9qUY4IMVfU-SX9RpTx-Yg', 1],
+    'OT':  ['11xLawls0LBEwRpp5lc130-nun2-Guh-7Gym7RSQ_-XU', 1],
+    'PT':  ['1EDk1za4PVYkUEgUVvjCH1CCEo1mtI5FXC-f7fhUMz8k', 1],
 
   },
 
   '2019-July': {
 
     'CLs': ['1r4kELYN33dsoxD6IGcivMsFGKSP5Qebci0iR8fMGOsI', 1],
-    'DE': ['12GULr80IX1HED0_ccG7fkaT4YZb_nrZKfGGDd5Vjy1U', 1],
-    'DL': ['1emjsj1mO4LqvKSBkWjWQQNmpQcrJQJauYsHW-0n7BT4', 1],
-    'OT': ['1QyFoBuVNdVvXiCqsiRF9vAg4Wzph0q6NwhgrNpfcqA4', 1],
-    'PT': ['11Hq6FpGAY8LTsdMgOiLjZJl2dwEa97A0eHpu1UNEEog', 1],
+    'DE':  ['12GULr80IX1HED0_ccG7fkaT4YZb_nrZKfGGDd5Vjy1U', 1],
+    'DL':  ['1emjsj1mO4LqvKSBkWjWQQNmpQcrJQJauYsHW-0n7BT4', 1],
+    'OT':  ['1QyFoBuVNdVvXiCqsiRF9vAg4Wzph0q6NwhgrNpfcqA4', 1],
+    'PT':  ['11Hq6FpGAY8LTsdMgOiLjZJl2dwEa97A0eHpu1UNEEog', 1],
 
   },
 
