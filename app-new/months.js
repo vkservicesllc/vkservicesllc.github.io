47723d726db2db54ceef1c21931a660459adbfae
vkservicesllc <35149642+vkservicesllc@users.noreply.github.com>
2021-08-06 13:31:58 -0400

    update 2108061331

--- app-new/months.js
+++ app-new/months.js
@@ -84,18 +84,18 @@ const monthlySpreadsheets = {
 
     cards: {
 
-      CL: ['1u47ZAPjmrkb4wjTtvDgVK-CWw2x7Odkdo5YnuKGu4Zg', 1, 1, 1],
-      CT: ['1tGJ13LJ1MiO6PX3SDTjbMorFAL4RdAfiSLdwwUKns3k', 1, 1, 1],
-      DE: ['1n9S7YVeAgxo_WXEGKrQz4cXqwIoRg_wgC5efTt-XUU4', 1, 1, 1],
+      CL: ['1u47ZAPjmrkb4wjTtvDgVK-CWw2x7Odkdo5YnuKGu4Zg', 0, 1, 1],
+      CT: ['1tGJ13LJ1MiO6PX3SDTjbMorFAL4RdAfiSLdwwUKns3k', 0, 1, 1],
+      DE: ['1n9S7YVeAgxo_WXEGKrQz4cXqwIoRg_wgC5efTt-XUU4', 0, 0, 0],
 
-      AC: ['1ml6wZ96V4GUznzUjNKYXZV542-EHKpaLwWSHS0KZDFM', 1, 1, 1],
-      AF: ['1cOBaPaQhcWuDsjCvcMSioMMBGRVrOtlrX8kGqkd2z7o', 1, 1, 1],
-      EE: ['1ORc-GnDbNKAC56sP4ZY8a4IEfte6GFyl5ZvkoTdufN0', 1, 1, 1],
-      SL: ['14ongzhULhPRT4qD-PoyjqGYyY1cvVwEZ3DnG0YPMaz8', 1, 1, 1],
-      LL: ['1qnbOvKYrKxg-gbbcd1dkjomq8F_o0hE4T7rtZbX7xqg', 1, 1, 1],
+      AC: ['1ml6wZ96V4GUznzUjNKYXZV542-EHKpaLwWSHS0KZDFM', 0, 0, 0],
+      AF: ['1cOBaPaQhcWuDsjCvcMSioMMBGRVrOtlrX8kGqkd2z7o', 0, 0, 0],
+      EE: ['1ORc-GnDbNKAC56sP4ZY8a4IEfte6GFyl5ZvkoTdufN0', 0, 0, 0],
+      SL: ['14ongzhULhPRT4qD-PoyjqGYyY1cvVwEZ3DnG0YPMaz8', 0, 0, 0],
+      LL: ['1qnbOvKYrKxg-gbbcd1dkjomq8F_o0hE4T7rtZbX7xqg', 0, 0, 0],
 
-      RT: ['14kUKjEqUqVLRfdRITOMKOfpwhZRu57GY42muJbVCoi0', 1, 1, 1],
-      OT: ['13OL29TE2C6bk0vFlDzuMCDNNpJ4w2vz_L0_XbyjU0I4', 1, 1, 1],
+      RT: ['14kUKjEqUqVLRfdRITOMKOfpwhZRu57GY42muJbVCoi0', 0, 0, 0],
+      OT: ['13OL29TE2C6bk0vFlDzuMCDNNpJ4w2vz_L0_XbyjU0I4', 0, 0, 0],
 
     },
 
